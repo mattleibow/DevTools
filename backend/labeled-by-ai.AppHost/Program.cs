a773b651c1bbffbeaf35fffaa4d74105e36a772a
Matthew Leibowitz <mattleibow@live.com>
2024-11-27 01:49:07 +0800

    local is optional

--- backend/labeled-by-ai.AppHost/Program.cs
+++ backend/labeled-by-ai.AppHost/Program.cs
@@ -1,26 +1,33 @@
 
-using Microsoft.AspNetCore.Mvc.Rendering;
-
 var builder = DistributedApplication.CreateBuilder(args);
 
+// Set this const to true in order to run this function and
+// AI locally without Azure hosting when doing development.
+const bool UseLocalAI = true;
+
+// Only use Application Insights in the published app.
 var insights = builder.ExecutionContext.IsPublishMode
     ? builder.AddAzureApplicationInsights("app-insights")
     : null;
 
-// use the Azure OpenAI in the published app,
-// but a local Ollama in development
+// Use the Azure OpenAI in the published app, but a local Ollama
+// in development or connect to an existing Azure OpenAI.
 IResourceBuilder<IResourceWithConnectionString> ai = builder.ExecutionContext.IsPublishMode
     ? builder.AddAzureOpenAI("openai")
         .AddDeployment(new("ai-model", "gpt-4o-mini", "2024-07-18", "Global Standard"))
-    : builder.AddOllama("ollama")
-        .WithDataVolume()
-        .WithContainerRuntimeArgs("--gpus=all")
-        .WithOpenWebUI()
-        .AddModel("llama3");
+    : UseLocalAI
+        ? builder.AddOllama("ollama")
+            .WithDataVolume()
+            .WithContainerRuntimeArgs("--gpus=all")
+            .WithOpenWebUI()
+            .AddModel("llama3")
+        : builder.AddConnectionString("openai");
 
+// Specify the storage for the function.
 var funcStorage = builder.AddAzureStorage("func-storage")
     .RunAsEmulator();
 
+// Export the function with public endpoints.
 var func = builder
     .AddAzureFunctionsProject<Projects.LabeledByAI>("labeled-by-ai")
     .WithExternalHttpEndpoints()
