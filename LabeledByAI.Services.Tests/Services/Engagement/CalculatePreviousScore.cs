6db668af96aca1ea358c0321b6fec39fdc8f6c47
Matthew Leibowitz <mattleibow@live.com>
2024-12-16 22:40:07 +0800

    Adding several more tests
    This helped detect the issue fixed in #42

---
+++ LabeledByAI.Services.Tests/Services/Engagement/CalculatePreviousScore.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+
+namespace LabeledByAI.Services.Tests;
+
+public partial class EngagementServiceUnitTests
+{
+    public class CalculatePreviousScore
+    {
+        EngagementService service = new(default!, default!);
+
+        [Fact]
+        public void BasicTestIssuePreviousScoreIsCorrect()
+        {
+            var issue = BasicExpectedIssues[0] with { TotalComments = 0, TotalReactions = 0 };
+
+            var score = service.CalculatePreviousScore(issue);
+
+            score.Should().Be(2);
+        }
+    }
+}
---
+++ LabeledByAI.Services.Tests/Services/Engagement/CalculateScoreAsync_Issue.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+
+namespace LabeledByAI.Services.Tests;
+
+public partial class EngagementServiceUnitTests
+{
+    public class CalculateScoreAsync_Issue
+    {
+        private readonly IGitHubConnection githubConnection;
+        private readonly EngagementService service;
+        private readonly GitHub github;
+
+        public CalculateScoreAsync_Issue()
+        {
+            githubConnection = GetConnection(
+                projectItems: BasicExpectedProjectItems,
+                issues: BasicExpectedIssues,
+                details: BasicExpectedDetails);
+
+            service = new(
+                githubConnection,
+                Substitute.For<ILogger<EngagementService>>());
+
+            github = new GitHub(githubConnection);
+        }
+
+        [Fact]
+        public async Task OldIssueHasCorrectScores()
+        {
+            var expectedIssue = BasicExpectedIssues[0];
+            var expectedResponseIssue = new EngagementResponseIssue(
+                expectedIssue.Id,
+                expectedIssue.Owner,
+                expectedIssue.Repository,
+                expectedIssue.Number);
+            var expectedResponseEngagement = new EngagementResponseEngagement(
+                Score: 11, 
+                PreviousScore: 5);
+
+            var response = await service.CalculateScoreAsync(
+                new EngagementRequestIssue(TestOwner, TestRepo, 1),
+                github);
+
+            response.Project.Should().BeNull();
+            response.TotalItems.Should().Be(1);
+            var item = response.Items.Should().ContainSingle().Subject;
+
+            item.Id.Should().BeNull();
+            item.Issue.Should().BeEquivalentTo(expectedResponseIssue);
+            item.Engagement.Should().BeEquivalentTo(expectedResponseEngagement);
+        }
+    }
+}
